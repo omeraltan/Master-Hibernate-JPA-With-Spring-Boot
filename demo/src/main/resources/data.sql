aaa05094dfceaca5aa3fa31f82333174f1b9dcdd
omeraltan <omeraltan66@gmail.com>
2024-03-11 14:30:03 +0300

    Commit All

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,21 @@
+/*
+ create table person
+(
+    id integer not null,
+    name varchar(255) not null,
+    location varchar(255),
+    birth_date timestamp,
+    primary key (id)
+);
+ */
+
+INSERT INTO PERSON
+(ID, NAME, LOCATION, BIRTH_DATE)
+VALUES(10001, 'Ranga', 'Hyderabad', '2022-12-31 23.59.59');
+INSERT INTO PERSON
+(ID, NAME, LOCATION, BIRTH_DATE)
+VALUES(10002, 'James', 'New York', '2022-12-31 23.59.59');
+INSERT INTO PERSON
+(ID, NAME, LOCATION, BIRTH_DATE)
+VALUES(10003, 'Pieter', 'Amsterdam', '2022-12-31 23.59.59');
+
