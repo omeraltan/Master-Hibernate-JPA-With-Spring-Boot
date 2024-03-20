e53beb425a5193472287f72b10750250b3cca63d
omeraltan <omeraltan66@gmail.com>
2024-03-20 09:59:33 +0300

    Commit All

---
+++ hibernate/src/main/java/com/jpa/hibernate/entity/FullTimeEmployee.java
@@ -0,0 +1,15 @@
+package com.jpa.hibernate.entity;
+
+import jakarta.persistence.Entity;
+
+import java.math.BigDecimal;
+
+@Entity
+public class FullTimeEmployee extends Employee{
+    protected FullTimeEmployee(){}
+    public FullTimeEmployee(String name, BigDecimal salary){
+        super(name);
+        this.salary = salary;
+    }
+    private BigDecimal salary;
+}
